214e7a75958c4d86a5091267015266b27701084e
Jodie Whitney <whitnejo@gmail.com>
2016-02-11 15:53:33 -0600

    Added .depend files to .gitignore. Added overload for - operator.

--- Overloading/main.cpp
+++ Overloading/main.cpp
@@ -9,13 +9,28 @@ public:
     {
         return length * breadth * height;
     }
+
+    double getLength()
+    {
+        return length;
+    }
     void setLength(double len)
     {
         length = len;
     }
+
+    double getBreadth()
+    {
+        return breadth;
+    }
     void setBreadth(double bre)
     {
         breadth = bre;
+    }
+
+    double getHeight()
+    {
+        return height;
     }
     void setHeight(double hei)
     {
@@ -31,6 +46,15 @@ public:
         return box;
     }
 
+    Box operator-(const Box& b)
+    {
+        Box box;
+        box.length = this->length - b.length;
+        box.breadth = this->breadth - b.breadth;
+        box.height = this->height - b.height;
+        return box;
+    }
+
 private:
     double length;
     double breadth;
@@ -42,6 +66,7 @@ int main()
     Box box1;
     Box box2;
     Box box3;
+    Box box4;
     double volume = 0.0;
 
     //box1 spec
@@ -54,16 +79,25 @@ int main()
     box2.setBreadth(13.0);
     box2.setHeight(10.0);
 
+    cout << "Box length: " << box1.getLength() << endl << "Box breadth: " << box1.getBreadth() << endl << "Box height: " << box1.getHeight() << endl << endl;
     volume = box1.getVolume();
-    cout << "box1 volume: " << volume << endl;
+    cout << "box1 volume: " << volume << endl << endl;
 
+    cout << "Box length: " << box2.getLength() << endl << "Box breadth: " << box2.getBreadth() << endl << "Box height: " << box2.getHeight() << endl << endl;
     volume = box2.getVolume();
     cout << "box2 volume: " << volume << endl;
 
     box3 = box1 + box2;
 
+    cout << "Box length: " << box3.getLength() << endl << "Box breadth: " << box3.getBreadth() << endl << "Box height: " << box3.getHeight() << endl << endl;
     volume = box3.getVolume();
-    cout << "box3 volume: " << volume << endl;
+    cout << "box3 volume: " << volume << endl << endl;
+
+    box4 = box2 - box1;
+
+    cout << "Box length: " << box4.getLength() << endl << "Box breadth: " << box4.getBreadth() << endl << "Box height: " << box4.getHeight() << endl << endl;
+    volume = box4.getVolume();
+    cout << "box4 volume: " << volume << endl << endl;
 
     cout << "Hey beautiful, love ya!" << endl;
     return 0;
