c8e1f55914b0bf90f6f82f4beb4f63214136cfc8
Jodie Whitney <whitnejo@gmail.com>
2016-02-11 19:05:11 -0600

    Implemented Shape class to be inherited by Rectangle and Triangle, updated README

--- PolyMorphism/main.cpp
+++ PolyMorphism/main.cpp
@@ -2,8 +2,60 @@
 
 using namespace std;
 
+class Shape {
+protected:
+    int width, height;
+
+public:
+    Shape(int w=0, int h=0)
+    {
+        width = w;
+        height = h;
+    }
+    int area()
+    {
+        cout << "Parent class area :" << endl;
+        return 0;
+    }
+};
+
+class Rectangle: public Shape{
+public:
+    Rectangle(int w=0, int h=0):Shape(w, h){}
+    int area()
+    {
+        cout << "Rectangle class area :" << endl;
+        return(width * height);
+    }
+};
+
+class Triangle: public Shape{
+public:
+    Triangle( int w=0, int h=0):Shape(w,h){}
+    int area()
+    {
+        cout << "Triangle class area :" <<endl;
+        return(width * height / 2);
+    }
+};
+
+// Main function for program
 int main()
 {
-    cout << "Hello world!" << endl;
+    Shape *shape;
+    Rectangle rec(10,7);
+    Triangle tri(10,5);
+
+    //store the address of Rectangle
+    shape = &rec;
+    //call rectangle area
+    shape->area();
+
+    //store the address of triangle
+    shape = &tri;
+    //call triangle area
+    shape->area();
+
+    cout << "Hey Beautiful, love ya!" << endl;
     return 0;
 }
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Playground of my experimental C++ projects
 
 Overloading 2-11-16
+
 Contains project focusing on overloading methods and operators.
